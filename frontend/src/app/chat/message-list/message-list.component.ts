c9d5aac60e0b33003a89eb112c313d820ed69a3a
Bennani, Mehdi <bennani.mehdi@courrier.uqam.ca>
2023-10-01 22:21:53 +0000

    Mis a jour du message-list pour le scrolling

--- frontend/src/app/chat/message-list/message-list.component.ts
+++ frontend/src/app/chat/message-list/message-list.component.ts
@@ -1,13 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input , OnInit, ElementRef, ViewChild, AfterViewChecked} from '@angular/core';
 import { Message } from '../message.model';
 @Component({
   selector: 'app-message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.css']
 })
-export class MessageListComponent {
+export class MessageListComponent implements  OnInit, AfterViewChecked {
   @Input() messages: Message[] | null = [];
+  
+  @ViewChild('chatContainer', { static: true }) myScrollContainer:
+    | ElementRef
+    | any;
 
+    ngOnInit(): void {
+      this.scrollToBottom();
+    }
+  
+    ngAfterViewChecked(): void {
+      this.scrollToBottom();
+    }
+  
+    scrollToBottom(): void {
+      try {
+        this.myScrollContainer.nativeElement.scrollTop =
+          this.myScrollContainer.nativeElement.scrollHeight;
+      } catch (err) {}
+    }
    /** Afficher la date seulement si la date du message précédent est différente du message courant. */
    showDateHeader(messages: Message[] | null, i: number) {
     if (messages != null) {
