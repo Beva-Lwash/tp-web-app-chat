fe3691f16bf543d0dba1b7e5bdfb783d44be01d3
Bennani, Mehdi <bennani.mehdi@courrier.uqam.ca>
2023-10-01 21:41:29 +0000

    Ajout du message-list component

--- frontend/src/app/chat/message-list/message-list.component.ts
+++ frontend/src/app/chat/message-list/message-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Message } from '../message.model';
 @Component({
   selector: 'app-message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent {
+  @Input() messages: Message[] | null = [];
 
+   /** Afficher la date seulement si la date du message précédent est différente du message courant. */
+   showDateHeader(messages: Message[] | null, i: number) {
+    if (messages != null) {
+      if (i === 0) {
+        return true;
+      } else {
+        const prev = new Date(messages[i - 1].timestamp).setHours(0, 0, 0, 0);
+        const curr = new Date(messages[i].timestamp).setHours(0, 0, 0, 0);
+        return prev != curr;
+      }
+    }
+    return false;
+  }
 }
