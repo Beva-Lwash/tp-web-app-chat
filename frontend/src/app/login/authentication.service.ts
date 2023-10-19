08ecd152614d489ff4813948fde835486c94a5e4
seye.ama <seye.ama@courrier.uqam.ca>
2023-10-19 14:41:22 -0400

    Partie 1 Etape C

--- frontend/src/app/login/authentication.service.ts
+++ frontend/src/app/login/authentication.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
+import { BehaviorSubject, Observable, firstValueFrom } from "rxjs";
 import { UserCredentials } from "./model/user-credentials";
 import { Router } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { LoginResponse } from "./model/LoginResponse";
+
 
 
 @Injectable({
@@ -13,12 +16,19 @@ export class AuthenticationService {
 
   private username = new BehaviorSubject<string | null>(null);
 
-  constructor(private router: Router){
+
+  constructor(private router: Router, private httpClient: HttpClient){
     this.username.next(localStorage.getItem(AuthenticationService.KEY));
+    this.httpClient=httpClient;
   }
   
 
-  login(userCredentials: UserCredentials) {
+  async login(userCredentials: UserCredentials) {
+    await firstValueFrom(
+      this.httpClient.post<LoginResponse>(`${environment.backendUrl}/auth/login`, userCredentials,
+    { withCredentials: true }
+    ))
+    ;
     const {username} = userCredentials;
     localStorage.setItem(AuthenticationService.KEY, username);
     this.username.next(username);
@@ -33,4 +43,6 @@ export class AuthenticationService {
   getUsername(): Observable<string | null> {
     return this.username.asObservable();
   }
+
+
 }
