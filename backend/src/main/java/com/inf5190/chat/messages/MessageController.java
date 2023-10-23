af9015841cfbe7e5fa0ff1c950ef4b23a5a7836c
Bennani, Mehdi <bennani.mehdi@courrier.uqam.ca>
2023-10-23 02:16:53 +0000

    Partie 3, Etape C

--- backend/src/main/java/com/inf5190/chat/messages/MessageController.java
+++ backend/src/main/java/com/inf5190/chat/messages/MessageController.java
@@ -6,11 +6,15 @@ import com.inf5190.chat.messages.repository.MessageRepository;
 import com.inf5190.chat.websocket.WebSocketManager;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 /**
  * Contrôleur qui gère l'API de messages.
