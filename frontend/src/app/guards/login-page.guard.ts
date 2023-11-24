2fc0c0a9adc04836d2c4488d3f6e5d0d13bb0d2e
seye.ama <seye.ama@courrier.uqam.ca>
2023-11-23 21:18:39 -0500

    Partie 1 Etape C en cours

--- frontend/src/app/guards/login-page.guard.ts
+++ frontend/src/app/guards/login-page.guard.ts
@@ -1,5 +1,11 @@
-import { CanActivateFn } from "@angular/router";
+import { inject } from "@angular/core";
+import { CanActivateFn, Router } from "@angular/router";
+import { AuthenticationService } from "../login/authentication.service";
 
 export const loginPageGuard: CanActivateFn = (route, state) => {
-  return true;
+  if (inject(AuthenticationService).isConnected()) {
+    return true;
+  } else {
+    return inject(Router).parseUrl("/");
+  }
 };
