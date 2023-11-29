838219c59feeb6ce5f8a961459d69df949f67d2c
seye.ama <seye.ama@courrier.uqam.ca>
2023-11-29 16:54:33 -0500

    Partie 1 Etape F correction

--- frontend/src/app/webSocketService.ts
+++ frontend/src/app/webSocketService.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Observable, Subject } from "rxjs";
+import { Observable, Subject, timeInterval, timeout } from "rxjs";
 import { environment } from "src/environments/environment";
 
 export type WebSocketEvent = "notif";
@@ -9,22 +9,32 @@ export type WebSocketEvent = "notif";
 })
 export class WebSocketService {
   private ws: WebSocket | null = null;
+  private retry: boolean;
+  private id: number;
 
-  constructor() {}
+  constructor() {
+    this.retry = false;
+  }
 
   public connect(): Observable<WebSocketEvent> {
     this.ws = new WebSocket(`${environment.wsUrl}/notifications`);
     const events = new Subject<WebSocketEvent>();
-
+    if (this.retry == true) {
+      clearTimeout(this.id);
+      events.complete();
+    }
     this.ws.onmessage = () => events.next("notif");
 
-    this.ws.onclose = () => setTimeout(this.connect, 2);
-    events.complete();
+    this.ws.onclose = () => {
+      this.id = window.setTimeout(this.connect, 2000);
+      this.retry = true;
+    };
 
     this.ws.onerror = () => events.error("error");
 
     return events.asObservable();
   }
+
   public disconnect() {
     this.ws?.close();
     this.ws = null;
