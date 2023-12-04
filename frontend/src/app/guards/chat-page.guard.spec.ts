bb32b634b80c4bb3540257543c5dc3f291c2a819
Bennani, Mehdi <bennani.mehdi@courrier.uqam.ca>
2023-12-04 00:07:51 +0000

    Update chat-page.guard.spec.ts

--- frontend/src/app/guards/chat-page.guard.spec.ts
+++ frontend/src/app/guards/chat-page.guard.spec.ts
@@ -1,23 +1,19 @@
-import { Injectable } from "@angular/core";
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
-import { AuthenticationService } from "../login/authentication.service";
+import { HttpClientModule } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
 
-@Injectable({
-  providedIn: "root",
-})
-export class ChatPageGuard implements CanActivate {
+import { ChatPageGuard } from './chat-page.guard';
 
-  constructor(
-    private authService: AuthenticationService,
-    private router: Router
-  ) {}
+describe('ChatPageGuard', () => {
+  let guard: ChatPageGuard;
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if (this.authService.isConnected()) {
-      return true;
-    } else {
-      this.router.navigate(["/"]); // Utilisez navigate au lieu de parseUrl pour naviguer vers la page d'accueil
-      return false;
-    }
-  }
-}
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
+    guard = TestBed.inject(ChatPageGuard);
+  });
+
+  it('should be created', () => {
+    expect(guard).toBeTruthy();
+  });
+});
